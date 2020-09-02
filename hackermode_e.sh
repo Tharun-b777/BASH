376706a9d18a91f071cff15d6becc50c421f5aa5
Tharun-b777 <tharunkumaar7@gmail.com>
2020-09-02 20:13:39 +0530

    added menu

--- hackermode_e.sh
+++ hackermode_e.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+function Del()
+{
+    files=$(ls /home/$1/Symptoms)
+    echo "enter number corresponding to file to delete"
+    select file in $files 
+    do 
+    rm /home/$1/Symptoms/$file 
+    exit
+    done 
+}
 #script for doc to delete symptom files
 read -p "Enter patient name " name 
-read -p "Enter symptomfile to be deleted eg(symptom_1.txt) " file 
+
 #check for doc name and patient
-if [ "$USER" == "doc1" ] && [ ! -d /home/${name}_1 ]
+if [ "$USER" == "doc1" ] && [ -d /home/${name}_1 ]
 then 
-rm /home/${name}_1/Symptoms/$file
+Del ${name}_1
 
-elif [ "$USER" == "doc2" ] && [ ! -d /home/${name}_2 ]
+elif [ "$USER" == "doc2" ] && [ -d /home/${name}_2 ]
 then
-rm /home/${name}_2/Symptoms/$file
+Del ${name}_2
 
-else 
+else
 echo patient dead
 fi
